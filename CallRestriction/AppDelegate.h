7cb76d291b4fd79f42827be39219082f7c83325e
Leslie Han <lesliehan9210@gmail.com>
2015-06-04 11:24:36 +0800

    Initial Commit

---
+++ CallRestriction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CallRestriction
+//
+//  Created by LeslieHan on 15/6/4.
+//  Copyright (c) 2015年 LeslieHan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CallRestriction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CallRestriction
+//
+//  Created by LeslieHan on 15/6/4.
+//  Copyright (c) 2015年 LeslieHan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
